a6fc07bb41581bf5f67448e661cebd6c6b116dba
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-11-04 11:14:07 +1100

    Exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -36,10 +36,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Contact.findById(request.params.id).then((contact) => {
-    response.json(contact);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        return response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -63,18 +69,58 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   persons = persons.filter((p) => p.id !== id);
-//   response.status(204).end();
-// });
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
-// app.get("/info", (request, response) => {
-//   let info = `<p>Phone book has info for ${persons.length} people.</p>`;
-//   const timeElapsed = Date.now();
-//   const requestTime = new Date(timeElapsed).toUTCString();
-//   response.send(info + requestTime);
-// });
+  const contact = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then((updatedContact) => {
+      response.json(updatedContact);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/info", (request, response, next) => {
+  Contact.find({})
+    .then((contacts) => {
+      const contactCount = contacts.length;
+      const info = `<p>Phone book has info for ${contactCount} people.</p>`;
+      const timeElapsed = Date.now();
+      const requestTime = new Date(timeElapsed).toUTCString();
+      response.send(info + requestTime);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformed id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
